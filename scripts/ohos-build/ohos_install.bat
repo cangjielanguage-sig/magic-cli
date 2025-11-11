0adba79024a2e0218db584827a8c55552df7315c
midwinter1993 <dongjie_cdj@163.com>
2025-11-11 15:17:33 +0800

    remove redundant file reads

--- scripts/ohos-build/ohos_install.bat
+++ scripts/ohos-build/ohos_install.bat
@@ -23,22 +23,23 @@ if "!DEV_ROOT!"=="" (
     set "DEV_ROOT=!DEV_ROOT!"
 )
 
-set /p workspace=Please Input Workspace(Create /Desktop/[Workspace] Dir On Harmony):  
+set /p workspace=Please Input Workspace(Create /Desktop/[Workspace] Dir On Harmony):
 
 call %DEV_ROOT%\cangjie-sdk-windows-1.0-ohos\cangjie\compiler\envsetup.bat
 
 :: save config files
 ren .\build.cj _build.cj
 ren .\cjpm.toml _cjpm.toml
-ren .\cjpm_ohos.toml cjpm.toml
-ren %MAGIC_PATH%\build.cj _build.cj
-ren %MAGIC_PATH%\cjpm.toml _cjpm.toml
-copy magic_cjpm.toml %MAGIC_PATH%\cjpm.toml
+copy /Y .\scripts\ohos-build\cjpm_ohos.toml cjpm.toml
+
+copy /Y %MAGIC_PATH%\build.cj %MAGIC_PATH%\_build.cj
+copy /Y %MAGIC_PATH%\cjpm.toml %MAGIC_PATH%\_cjpm.toml
+copy /Y .\scripts\ohos-build\magic_cjpm.toml %MAGIC_PATH%\cjpm.toml
 
 :: echo COMPILE librawinput.so...
 cjpm clean
-%DEV_ROOT%\deveco-studio-5.1.1.804CJ.win\deveco-studio\sdk\default\openharmony\native\llvm\bin\clang.exe -B %DEV_ROOT%/cangjie-sdk-windows-1.0-ohos/cangjie/compiler/third_party/llvm/bin -B %DEV_ROOT%/deveco-studio-5.1.1.804CJ.win/deveco-studio/sdk/default/openharmony/native/sysroot/usr/lib/aarch64-linux-ohos -L %DEV_ROOT%/deveco-studio-5.1.1.804CJ.win/deveco-studio/sdk/default/openharmony/native/sysroot/usr/lib/aarch64-linux-ohos -L %DEV_ROOT%/deveco-studio-5.1.1.804CJ.win/deveco-studio/sdk/default/openharmony/native/llvm/lib/clang/15.0.4/lib/aarch64-linux-ohos -L %DEV_ROOT%/deveco-studio-5.1.1.804CJ.win/deveco-studio/sdk/default/openharmony/native/llvm/lib/aarch64-linux-ohos --sysroot %DEV_ROOT%/deveco-studio-5.1.1.804CJ.win/deveco-studio/sdk/default/openharmony/native/sysroot -c ffi/raw_input_linux.c -o ffi/raw_input_linux_static.o -target aarch64-linux-ohos  
-%DEV_ROOT%/cangjie-sdk-windows-1.0-ohos/cangjie/compiler/third_party/llvm/bin/llvm-ar.exe rcs ffi/librawinput.a ffi/raw_input_linux_static.o
+@REM %DEV_ROOT%\deveco-studio-5.1.1.804CJ.win\deveco-studio\sdk\default\openharmony\native\llvm\bin\clang.exe -B %DEV_ROOT%/cangjie-sdk-windows-1.0-ohos/cangjie/compiler/third_party/llvm/bin -B %DEV_ROOT%/deveco-studio-5.1.1.804CJ.win/deveco-studio/sdk/default/openharmony/native/sysroot/usr/lib/aarch64-linux-ohos -L %DEV_ROOT%/deveco-studio-5.1.1.804CJ.win/deveco-studio/sdk/default/openharmony/native/sysroot/usr/lib/aarch64-linux-ohos -L %DEV_ROOT%/deveco-studio-5.1.1.804CJ.win/deveco-studio/sdk/default/openharmony/native/llvm/lib/clang/15.0.4/lib/aarch64-linux-ohos -L %DEV_ROOT%/deveco-studio-5.1.1.804CJ.win/deveco-studio/sdk/default/openharmony/native/llvm/lib/aarch64-linux-ohos --sysroot %DEV_ROOT%/deveco-studio-5.1.1.804CJ.win/deveco-studio/sdk/default/openharmony/native/sysroot -c ffi/raw_input_linux.c -o ffi/raw_input_linux_static.o -target aarch64-linux-ohos
+@REM %DEV_ROOT%/cangjie-sdk-windows-1.0-ohos/cangjie/compiler/third_party/llvm/bin/llvm-ar.exe rcs ffi/librawinput.a ffi/raw_input_linux_static.o
 cjpm build --target aarch64-linux-ohos -i -o magic-cli -V
 
 hdc target mount
@@ -62,8 +63,9 @@ hdc file send "ohos\cl100k_base.tiktoken" /storage/media/100/local/files/Docs/De
 
 :: resume config files
 ren .\_build.cj build.cj
-ren .\cjpm.toml cjpm_ohos.toml
+del /F /Q .\cjpm.toml
 ren .\_cjpm.toml cjpm.toml
+
 ren %MAGIC_PATH%\_build.cj build.cj
-del /q %MAGIC_PATH%\cjpm.toml
+del /F /Q %MAGIC_PATH%\cjpm.toml
 ren %MAGIC_PATH%\_cjpm.toml cjpm.toml
